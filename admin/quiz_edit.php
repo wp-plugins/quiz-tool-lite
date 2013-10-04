cbd0457959b151ff0144da02325c873f7a6442a8
alexfurr <alexfurr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-04 10:02:44 +0000

    Latest version checkin
    git-svn-id: https://plugins.svn.wordpress.org/quiz-tool-lite/tags/1.4@782487 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/quiz_edit.php
@@ -0,0 +1,200 @@
+<?php
+
+$quizID="";
+$feedback="";
+$questionArray="";
+$quizName="";
+$quizOptionsArray=array();
+
+if(isset($_GET['action']))
+{
+	$action=$_GET['action'];
+	
+	if($action=="quizEdit")
+	{
+		$feedback =  '<span class="successText">Quizzes updated</span>';
+		$quizID= quizEdit();
+	}
+
+
+}
+
+
+if(isset($_GET['quizID']))
+{
+	$quizID= $_GET['quizID'];	
+}
+
+
+if($quizID=="")
+{
+	if(isset($_POST['quizID']))
+	{
+		$quizID = $_POST['quizID'];
+	}
+	
+}
+
+
+if($quizID)
+{
+	$quizInfo = getQuizInfo($quizID);
+	$quizName = utils::convertTextFromDB($quizInfo['quizName']);
+	$questionArray = $quizInfo['questionArray'];
+	$quizOptionsArray = $quizInfo['quizOptions'];
+	
+	// Unserialise the array
+	$questionArray = unserialize($questionArray);
+	$quizOptionsArray = unserialize($quizOptionsArray);	
+	
+	
+}
+else
+{
+	$quizOptionsArray['showFeedback']="yes";
+}
+
+
+
+
+?>
+
+<h1>Edit Quiz</h1>
+
+
+<a href="admin.php?page=ai-quiz-quiz-list" class="backIcon">Return to my quizzes</a>
+
+
+    <hr/>
+<?php 
+if($feedback)
+{
+	echo '<div id="feedback">'.$feedback.'</div>';
+}
+?>
+    
+    <form action="admin.php?page=ai-quiz-quiz-edit&action=quizEdit" method="post">
+    <label for ="quizName">Quiz Name</label>
+    <input type="text" name="quizName" id="quizName" value="<?php echo $quizName?>">
+
+    <hr/>
+    
+	<?php
+	// Now get the question posts, count the questinos and add drop down options
+	echo '<table>';
+	$potRS = getQuestionPots();
+	
+	foreach ($potRS	as $myPots)
+	{
+		
+		$potName = stripslashes($myPots['potName']);
+		$potID= $myPots['potID'];	
+		
+		// Ge tthe number of questinos form this pot, if any
+		$qCountFromPot="";		
+		
+		if(isset($questionArray[$potID]))
+		{
+			$qCountFromPot = $questionArray[$potID];
+		}
+		
+		// Get the question Count from those pots
+		$questionRS = getQuestionsInPot($potID, false); // fasl referes to ignoring the reflcetion questions
+		$questionCount = count($questionRS);
+		
+		echo '<tr>';
+		echo '<td>'.$potName.'</td>';
+		echo '<td width="10">';
+		echo '<select name="potID'.$potID.'">';
+		$i=0;
+		while($questionCount>=$i)
+		{
+			echo '<option value="'.$i.'"';
+			if($qCountFromPot==$i){echo ' selected';}
+			echo '>';
+			echo $i;
+			echo '</option>';
+			$i++;
+			
+		}
+		echo '</select>';
+		echo '</td>';
+		echo '<td>';
+		echo 'questions from this pot';
+		echo '</td>';
+		echo '</tr>';
+	}
+	echo '</table>';
+    ?>
+    <hr/>
+    <h3>Quiz Options</h3>  
+    <table>
+    <tr>
+  		<td>
+			<label for="startDate">Start Date</label>
+		</td>
+		<td>			
+			<input type="text" class="MyDate" name="startDate" id="startDate" size="8" value="<?php echo $quizOptionsArray['startDate']; ?>"/><br> 
+		</td>
+	</tr>
+	<tr>	
+		<td>	
+			<label for="endDate">End Date</label>
+		</td>
+		<td>
+			<input type="text" class="MyDate" name="endDate" id="endDate" size="8" value="<?php echo $quizOptionsArray['endDate']; ?>"/><br>   
+		</td>
+	</tr>
+	<tr>	
+		<td>	   
+			<label for="maxAttempts">Max number of attempts</label>
+		</td>
+		<td>
+			<input type="text" name="maxAttempts" id="maxAttempts"  size="3" value="<?php echo $quizOptionsArray['maxAttempts']; ?>"/><br> 
+		</td>
+	</tr>
+	<tr>	
+		<td>	
+			Display feedback
+
+		</td>
+		<td>
+			<label for="showFeedbackYes">Yes</label>
+			<input type="radio" name="showFeedback" id="showFeedbackYes"  value="yes"  <?php if ($quizOptionsArray['showFeedback']=='yes'){echo 'checked'; }?>/>
+			<label for="showFeedbackNo">No</label>
+			<input type="radio" name="showFeedback" id="showFeedbackNo"  value="no" <?php if ($quizOptionsArray['showFeedback']=='no'){echo 'checked'; }?>/><br> 
+		</td>
+	</tr>
+    <tr>
+    <td><label for="requireUserLoggedIn">Require user to be logged in</label></td>
+    <td>
+    <input type="checkbox" name="requireUserLoggedIn" id="requireUserLoggedIn" <?php if ($quizOptionsArray['requireUserLoggedIn']=='on'){echo 'checked'; }?>/>
+    </td>
+    </tr>
+	<tr>	
+		<td>
+			Minimum time between attempts
+		</td>
+		<td>
+			<select name="timeAttemptsHour" id="timeAttemptsHour">
+			<?php
+			$hourRange = range(0, 24, 1);
+			foreach ($hourRange as $hour) {
+			  	//echo "<option value='$hour'>$hour </option>";
+			   	echo "<option value='$hour'";
+			   	if(($quizOptionsArray[timeAttemptsHour]==$hour)){
+			   	 	echo 'selected';
+			   	}
+			   	echo ">$hour </option>";			  
+			}
+			?>
+			</select><label for="timeAttemptsDay">Hour(s)</label> 
+			<input type="text" name="timeAttemptsDay" id="timeAttemptsDay"  size="3" value="<?php echo $quizOptionsArray[timeAttemptsDay]; ?>"/>
+			<label for="timeAttemptsDay">Day(s)</label>
+	</td>
+
+	</table>
+    <hr/>
+    <input type="hidden" value="<?php echo $quizID?>" name="quizID" />
+    <input type="submit" value="Update Quiz" class="button-primary" />
+</form>
---
+++ functions.php
@@ -0,0 +1,328 @@
+<?php
+
+
+// Add the Admin Menu Items
+function AI_Quiz_createAdminMenu() {
+	
+	$myIcon = plugins_url();
+	$myIcon.='/quiz-tool-lite/images/quiz_icon.png';
+	
+	// Create main menu item
+	$page_title="Quiz Questions";
+	$menu_title="Quiz Questions";
+	$capability="administrator";
+	$menu_slug="ai-quiz-home";
+	$function="drawAIquiz_home";
+	$iconURL=$myIcon;	
+	add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $iconURL);
+	
+	$parentSlug = "ai-quiz-home";
+	$page_title="Quizzes";
+	$menu_title="Quizzes";
+	$capability="administrator";
+	$menu_slug="ai-quiz-quiz-list";
+	$function="drawAIquiz_QuizList";
+	add_submenu_page($parentSlug, $page_title, $menu_title, $capability, $menu_slug, $function);
+	
+	$parentSlug = "ai-quiz-question-list";
+	$page_title="Question List";
+	$menu_title="Question List";
+	$capability="administrator";
+	$menu_slug="ai-quiz-question-list";
+	$function="drawAIquiz_questionList";
+	add_submenu_page($parentSlug, $page_title, $menu_title, $capability, $menu_slug, $function);	
+	
+	$parentSlug = "ai-quiz-quiz-list";
+	$page_title="Edit Quiz";
+	$menu_title="Edit Quiz";
+	$capability="administrator";
+	$menu_slug="ai-quiz-quiz-edit";
+	$function="drawAIquiz_quizEdit";
+	add_submenu_page($parentSlug, $page_title, $menu_title, $capability, $menu_slug, $function);			
+
+
+	
+	$parentSlug = "ai-quiz-question-list";
+	$page_title="Edit Question";
+	$menu_title="Edit Question";
+	$capability="administrator";
+	$menu_slug="ai-quiz-question-edit";
+	$function="drawAIquiz_questionEdit";
+	add_submenu_page($parentSlug, $page_title, $menu_title, $capability, $menu_slug, $function);
+	
+	$parentSlug="ai-quiz-home";
+	$page_title="Results";
+	$menu_title="Results";
+	$capability="administrator";
+	$menu_slug="ai-quiz-results";
+	$function="drawAIquiz_results";
+	add_submenu_page($parentSlug, $page_title, $menu_title, $capability, $menu_slug, $function);	
+	
+	$parentSlug="ai-quiz-questionType";
+	$page_title="Pick Question Type";
+	$menu_title="Pick Question Type";
+	$capability="administrator";
+	$menu_slug="ai-quiz-questionType";
+	$function="drawAIquiz_questionType";
+	add_submenu_page($parentSlug, $page_title, $menu_title, $capability, $menu_slug, $function);		
+	
+	$parentSlug="ai-quiz-home";
+	$page_title="Settings";
+	$menu_title="Settings";
+	$capability="administrator";
+	$menu_slug="ai-quiz-settings";
+	$function="drawAIquiz_settings";
+	add_submenu_page($parentSlug, $page_title, $menu_title, $capability, $menu_slug, $function);	
+	
+	$parentSlug="ai-quiz-home";
+	$page_title="Export Quiz Questions";
+	$menu_title="Export Quiz Questions";
+	$capability="administrator";
+	$menu_slug="ai-quiz-export";
+	$function="drawAIquiz_export";
+	add_submenu_page($parentSlug, $page_title, $menu_title, $capability, $menu_slug, $function);
+	
+	$parentSlug="ai-quiz-home";
+	$page_title="Help";
+	$menu_title="Help";
+	$capability="administrator";
+	$menu_slug="ai-quiz-help";
+	$function="drawAIquiz_help";
+	add_submenu_page($parentSlug, $page_title, $menu_title, $capability, $menu_slug, $function);	
+}
+
+
+
+  
+function AI_Quiz_loadMyPluginScripts()
+{  
+	// Include JS/CSS only if we're on our options page  
+	if (AI_Quiz_isMyPluginScreen())
+	{  
+		wp_enqueue_script('js_custom', plugins_url('/scripts/js-functions.js',__FILE__) ); #Custom JS functions
+
+		wp_register_style( 'QTL_css_custom',  plugins_url('/css/styles.css',__FILE__) );
+        wp_enqueue_style( 'QTL_css_custom' );
+		
+		wp_register_style( 'QTL_css_icons',  plugins_url('/css/icons.css',__FILE__) );
+        wp_enqueue_style( 'QTL_css_icons' );	
+	} 
+} 
+
+function my_admin_init()
+{
+	$pluginfolder = plugins_url();
+	wp_enqueue_script('jquery');
+	wp_enqueue_script('jquery-ui-core');
+	wp_enqueue_script('jquery-ui-datepicker');
+		
+	// tell WordPress to load the Smoothness theme from Google CDN
+	$url = "https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.7/themes/smoothness/jquery-ui.css";
+	wp_enqueue_style('jquery-ui-smoothness', $url, false, null);		
+}
+add_action('admin_init', 'my_admin_init');
+
+
+function my_admin_footer() {
+	    ?>
+	    <script type="text/javascript">
+	    jQuery(document).ready(function(){
+	        jQuery('.MyDate').datepicker({
+	            dateFormat : 'yy-mm-dd'
+	        });
+	    });
+	    </script>
+	    <?php
+	}
+add_action('admin_footer', 'my_admin_footer');
+ 
+ 
+// Check if we're on our options page   and only load plugin scripts if so
+function AI_Quiz_isMyPluginScreen()
+{
+	
+	global $hook_suffix;
+	global $overrideAdminCheck; //override for loading up gplugin scripts from front end i.e. doesn't need to check if on admin page
+	
+	if($overrideAdminCheck==true)
+	{
+		return true;
+	}
+	else
+	{
+	
+		$myPluginPages = array(
+		"toplevel_page_ai-quiz-home",
+		"admin_page_ai-quiz-question-list",
+		"quiz-questions_page_ai-quiz-quiz-list",
+		"admin_page_ai-quiz-quiz-edit",
+		"quiz-questions_page_ai-quiz-results",
+		"admin_page_ai-quiz-question-edit",
+		"admin_page_ai-quiz-questionType",
+		"quiz-questions_page_ai-quiz-export",
+		"quiz-questions_page_ai-quiz-settings",
+		"quiz-questions_page_ai-quiz-help",
+		"ai-quiz-results_uos"	
+		);
+		
+		
+		$screen = get_current_screen();
+		
+		$thisPage = $screen->id;
+		
+		if (in_array($thisPage, $myPluginPages)) {
+			$isMyPluginPage = true;
+		}
+		else
+		{
+			$isMyPluginPage = false;
+		}
+		
+		
+		if (is_object($screen) && $isMyPluginPage==true)
+		{
+			return true;  
+		}else{  
+			return false;  
+		}  
+	}
+} 
+
+
+
+// Adds the ADD QUESTION option to posts and pages in the editor
+class AIQuiz_TinyMCE_Button 
+{	
+	
+	static public function tinymce_add_button()
+	{
+		if ( ! current_user_can('edit_posts') && ! current_user_can('edit_pages') )
+			return;
+
+		if ( get_user_option('rich_editing') == 'true') 
+		{
+			add_filter("mce_external_plugins", array("AIQuiz_TinyMCE_Button","tinymce_custom_plugin"));
+			add_filter('mce_buttons', array("AIQuiz_TinyMCE_Button",'tinymce_register_button'));
+		}
+	}
+		 
+	static public function tinymce_register_button($buttons) 
+	{
+		array_push($buttons, "|", "AIquizButtonAdd");
+		return $buttons;
+	}
+		 
+	static public function tinymce_custom_plugin($plugin_array) 
+	{
+		$plugin_array['AIquizButtonAdd'] = WP_PLUGIN_URL.'/quiz-tool-lite/mce/editor_plugin.js';
+		return $plugin_array;
+	}
+	
+	static public function addAI_Button($atts)
+	{
+		if($atts['id'])
+		{
+			$id = $atts['id'];
+			$width = $atts['width']?$atts['width']:640;
+			$height = $atts['height']?$atts['height']:385;
+		}
+	}
+	
+}
+
+add_action('init', array('AIQuiz_TinyMCE_Button','tinymce_add_button'));
+add_shortcode('kkytv', array('AIQuiz_TinyMCE_Button','addAI_Button'));
+// End of Tiny MCE add question icon to bar
+
+
+
+
+
+
+
+// farbtastic code is required for the colour picker wheel
+add_action('init', 'load_scripts');
+function load_scripts() {
+	wp_enqueue_style( 'farbtastic' );
+	wp_enqueue_script( 'farbtastic' );
+}
+
+
+
+
+if (!class_exists('DownloadCSV'))
+{
+	class DownloadCSV
+	{
+		static function on_load()
+		{
+			add_action('plugins_loaded',array(__CLASS__,'plugins_loaded'));
+			register_activation_hook(__FILE__,array(__CLASS__,'activate'));
+	    }
+	
+		static function plugins_loaded()
+		{
+			global $pagenow;
+			
+			
+			if ( current_user_can( 'manage_options' ) )
+	//	{) // Are they logged in?
+			{
+				
+				
+				$downloadType="";
+				if(isset($_GET['download']))
+				{
+					$downloadType = $_GET['download'];
+				}
+				
+				
+				if ($pagenow=='admin.php' && $downloadType=='csv')
+				{
+					$fileName = 'quizQuestionsExport.csv';
+					 
+					header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+					header('Content-Description: File Transfer');
+					header("Content-type: text/csv");
+					header("Content-Disposition: attachment; filename={$fileName}");
+					header("Expires: 0");
+					header("Pragma: public");
+					
+					$fh = @fopen( 'php://output', 'w' );
+					
+					$CSV_array = AI_Quiz_importExport::getQuestionCSVData();
+	 
+					// Use the keys from $data as the titles
+					//fputcsv($fh, $CSV_array);
+					
+					foreach ($CSV_array as $fields) {
+						fputcsv($fh, $fields);
+					}				
+					
+					// Close the file
+					fclose($fh);
+					// Make sure nothing else is sent, our file is done
+					exit;
+				}
+			}
+		}
+	}
+	
+	//if (is_user_logged_in()) // Are they logged in?
+	//{
+	//	if ( current_user_can( 'manage_options' ) )
+	//	{
+			/* A user with admin privileges */
+			DownloadCSV::on_load();
+	//	}
+	//}
+}
+
+
+// Allow the poopup thickbox to appear all pages.
+add_thickbox(); 
+
+
+
+
+?>
